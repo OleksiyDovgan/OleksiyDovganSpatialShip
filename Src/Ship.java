b5c6ca8e1cfcd8e76b67256910e49de4847f15bf
OleksiyDovgan <oleksiy.dovgan@gmail.com>
2023-08-10 16:18:43 +0200

    code numbers of 100 spaceship

---
+++ Src/Ship.java
@@ -0,0 +1,189 @@
+import java.util.Arrays;
+
+public class Ship {
+
+    public static void main(String[] args) {
+
+        double shn = 1;
+
+        while (shn < 121){
+
+            System.out.println(shn);
+
+            shn++;
+
+            if(shn == 4){
+
+                shn = shn - 0.5;
+
+            }if(shn == 4.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 9){
+
+                shn = shn - 0.5;
+
+            }if(shn == 9.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 14){
+
+                shn = shn - 0.5;
+
+            }if(shn == 14.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 19){
+
+                shn = shn - 0.5;
+
+            }if(shn == 19.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 24){
+
+                shn = shn - 0.5;
+
+            }if(shn == 24.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 29){
+
+                shn = shn - 0.5;
+
+            }if(shn == 29.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 34){
+
+                shn = shn - 0.5;
+
+            }if(shn == 34.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 39){
+
+                shn = shn - 0.5;
+
+            }if(shn == 38.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 40){
+
+                shn = shn + 10;
+
+            }if(shn == 54){
+
+                shn = shn - 0.5;
+
+            }if(shn == 54.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 59){
+
+                shn = shn - 0.5;
+
+            }if(shn == 59.5){
+
+                shn = shn +0.5;
+
+            }if(shn == 64){
+
+                shn = shn - 0.5;
+
+            }if(shn == 64.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 69){
+
+                shn = shn - 0.5;
+
+            }if(shn == 69.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 74){
+
+                shn = shn - 0.5;
+
+            }if(shn == 74.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 79){
+
+                shn = shn - 0.5;
+
+            }if(shn == 79.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 84){
+
+                shn = shn - 0.5;
+
+            }if(shn == 84.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 89){
+
+                shn = shn - 0.5;
+
+            }if(shn == 89.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 90){
+
+                shn=shn+10;
+
+            }if(shn == 104){
+
+                shn = shn - 0.5;
+
+            }if(shn == 104.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 109){
+
+                shn = shn - 0.5;
+
+            }if(shn == 109.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 114){
+
+                shn = shn - 0.5;
+
+            }if(shn == 114.5){
+
+                shn = shn + 0.5;
+
+            }if(shn == 119){
+
+                shn = shn - 0.5;
+
+            }if(shn == 119.5){
+
+                shn = shn + 0.5;
+            }
+        }
+
+        System.out.println("Here are the code numbers of 100 spaceship");
+
+    }
+
+}
